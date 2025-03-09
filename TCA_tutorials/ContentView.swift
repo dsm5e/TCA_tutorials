6e7630948837115775761182ff90ffe44351b9ca
dsm5e <justry24@gmail.com>
2025-03-09 13:43:42 +0300

    Initial

---
+++ TCA_tutorials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA_tutorials
+//
+//  Created by dsm 5e on 09.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA_tutorials/TCA_tutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_tutorialsApp.swift
+//  TCA_tutorials
+//
+//  Created by dsm 5e on 09.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TCA_tutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
